067ba625e597066821d8b57e9fee60a4556a477b
SzabolcsMarton <maszabolcs@gmail.com>
2022-03-05 12:37:02 +0100

    Phonebook solved

---
+++ src/main/java/phonebook/Phonebook.java
@@ -0,0 +1,40 @@
+package phonebook;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        ensureParamsNotNull(contacts, output);
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
+            writeFromMap(contacts, writer);
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file", ioe);
+        }
+    }
+
+    private void writeFromMap(Map<String, String> contacts, BufferedWriter writer) throws IOException {
+        for (Map.Entry<String,String> actual : contacts.entrySet()){
+            writer.write(actual.getKey()+ ": " + actual.getValue());
+            writer.newLine();
+        }
+    }
+
+
+    private void ensureParamsNotNull(Map<String, String> contacts, String output) {
+        if (contacts == null ) {
+            throw new IllegalArgumentException("Contact is null");
+        }
+        if (output == null ) {
+            throw new IllegalArgumentException("Output is null");
+        }
+    }
+
+
+}
